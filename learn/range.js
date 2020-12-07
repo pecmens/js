a4292dfb9e717e268d16b65f60b961fe42f11ea3
pecmens@hotmail.com <pecmens@hotmail.com>
2020-12-08 03:14:55 +0800

    range for javascript

--- learn/range.js
+++ learn/range.js
@@ -1,2 +1,2 @@
-const range = (start, stop, step = 1) => Array(Math.cell((stop - start) / step)).fill(start).map((x, y) => x + y * step)
+const range = (start, stop, step = 1) => Array(Math.ceil((stop - start) / step)).fill(start).map((x, y) => x + y * step)
 console.log(range(1, 101));
