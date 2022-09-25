66bef50bc44ef2e7e5aef9c7ee6829a17586a0ea
pecmens <pecmens@hotmail.com>
2022-09-25 18:00:26 +0800

    screen first limted 8 bit

--- project/calc/main.js
+++ project/calc/main.js
@@ -1,9 +1,18 @@
 let opt = document.querySelector("#t_show");
 let opb = document.querySelector("#b_show");
+//If Executed the function is over, screen values is locked and press
+//any key to replace it with new values.
+let flag = 0;
 
 function press(key) {
-    if (opb.innerHTML === "0") {
+    if (opb.innerHTML === "0" || flag === 1) {
+        flag = 0;
         opb.innerHTML = key.innerHTML;
+    } else if (opb.innerHTML.length > 8 && !(opb.innerHTML.indexOf("+") !== -1 || opb.innerHTML.indexOf("-") !== -1 || opb.innerHTML.indexOf("*") !== -1 || opb.innerHTML.indexOf("/") !== -1 || opb.innerHTML.indexOf("%") !== -1)) {
+        opb.innerHTML += "";
+        console.log("Values is Screen Overflow!")
+    } else if (opb.innerHTML.length > 17) {
+        opb.innerHTML += "";
     } else {
         opb.innerHTML += key.innerHTML;
     }
@@ -12,6 +21,7 @@ function press(key) {
 function clean() {
     opt.innerHTML = "";
     opb.innerHTML = "0";
+    flag = 0;
 }
 
 
@@ -33,6 +43,7 @@ function executive() {
         result = "Error!";
     }
     opb.innerHTML = result;
+    flag = 1;
 }
 
 
@@ -43,6 +54,7 @@ function operator(key) {
     } else {
         opb.innerHTML += key.innerHTML;
     }
+    flag = 0;
 }
 
 
